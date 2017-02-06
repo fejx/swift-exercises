8d5ab95f04d90170e87a41135dad7fd0071dfb5e
fej <ff.jj@gmx.at>
2017-02-06 12:49:18 +0100

    Add dummy implementation for CGRect

---
+++ exam/exam4/Sources/GCRect.swift
@@ -0,0 +1,11 @@
+// dummy implementation for CGRect
+struct CGRect {
+    let x, y, width, height: Int
+
+    init(x: Int, y: Int, width: Int, height: Int) {
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    }
+}
